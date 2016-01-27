e8d4f26765242485f4a4f3bd051d5bec29192337
michael smith and neil larion <neil.larion@gmail.com>
2016-01-27 11:40:44 -0800

    Add Chai-things.js for testing and update spec-runner to test game and board objects.

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,33 @@ Space.prototype.markedBy = function (player) {
     return false;
   }
 };
+
+var Board = function() {
+  var array = [];
+
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      var space = new Space(i, j);
+      array.push(space);
+    }
+  }
+  return array;
+}
+
+var Game = function () {
+  this.level = 0;
+};
+
+Game.prototype.makeBoard = function () {
+  this.board = new Board();
+  return this.board;
+};
+
+Game.prototype.makePlayers = function (players) {
+  if(players === 1){
+    this.player1 = new Player("X");
+  } else if (players === 2) {
+    this.player1 = new Player("X");
+    this.player2 = new Player("O");
+  }
+};
--- spec/specs.js
+++ spec/specs.js
@@ -27,8 +27,46 @@ describe('Space', function() {
 });
 
 describe('Board', function() {
-  it("creates 9 spaces when it is initialized", function() {
-    var testBoard = new Board(9);
-    expect(testBoard.spaces).to.eql([{},{},{},{},{},{},{},{},{}]);
+  it("creates spaces when it is initialized", function() {
+    var testBoard = new Board();
+    expect(testBoard.length).to.equal(9);
    });
+
+   it("creates spaces with x and y keys that are 0,0 for first object", function() {
+     var testBoard = new Board();
+     expect(testBoard).to.have.deep.property('[0].x',0);
+     expect(testBoard).to.have.deep.property('[0].y',0);
+
+  });
+
+  it("creates spaces with x and y keys", function() {
+    var testBoard = new Board();
+    testBoard.should.all.have.property('x');
+    testBoard.should.all.have.property('y');
+   });
+
+   it("creates spaces and marks the first space with player x", function() {
+     var testBoard = new Board();
+     var testPlayer = new Player("X");
+     testBoard[0].markedBy(testPlayer);
+     expect(testBoard[0].mark).to.equal(testPlayer);
+    });
+});
+
+describe('Game', function () {
+  it("initializes the board", function () {
+    var testGame = new Game();
+    var testBoard = new Board();
+    expect(testGame.makeBoard()).to.eql(testBoard);
+  });
+  it("initializes the players", function () {
+    var testGame = new Game();
+    var testPlayer1 = new Player("X");
+    var testPlayer2 = new Player("O");
+    testGame.makePlayers(1);
+    expect(testGame.player1).to.eql(testPlayer1);
+    testGame.makePlayers(2);
+    expect(testGame.player1).to.eql(testPlayer1);
+    expect(testGame.player2).to.eql(testPlayer2);
+  });
 });
